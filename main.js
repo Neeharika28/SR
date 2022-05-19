146691daccc9b25975ce50b7bc6939b8ef55423a
Neeharika28 <74006198+Neeharika28@users.noreply.github.com>
2022-05-19 16:58:35 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -23,31 +23,27 @@ function gotResults(error, results) {
         green = Math.floor(Math.random() * 255) + 1;
         blue = Math.floor(Math.random() * 255) + 1;
 
-        document.getElementById("results_count").innerHTML = "Dog = " + dog, "Cat = " + cat, "Lion = " + lion, "Bird" + bird;
+        document.getElementById("results_count").innerHTML = "Dog = " +  dog  + '<br>' +  "Cat = " +  cat + '<br>' + "Lion = " + lion + '<br>' + "Bird = " + bird;
         document.getElementById("results_label").innerHTML = "Detected sound is of - " + results[0].label;
         document.getElementById("results_count").style.color = "rgb(" + red + "," + green + "," + blue + ")";
         document.getElementById("results_label").style.color = "rgb(" + red + "," + green + "," + blue + ")";
 
-        img = document.getElementById("dog gif.gif");
-        img1 = document.getElementById("cat gif.gif");
-        img2 = document.getElementById("lion gif.gif");
-        img3 = document.getElementById("bird gif.gif");
-        img4 = document.getElementById("hear.jpg");
+        img = document.getElementById("pic");
 
         if (results[0].label == "Dog") {
             img.src = "dog gif.gif";
             dog = dog + 1;
         } else if (results[0].label == "Cat") {
-            img2.src = "cat gif.gif";
+            img.src = "cat gif.gif";
             cat = cat + 1;
         } else if (results[0].label == "Lion") {
-            img3.src = "lion gif.gif";
+            img.src = "lion gif.gif";
             lion = lion + 1;
         } else if (results[0].label == "Bird") {
-            img4.src = "bird gif.gif"
+            img.src = "bird gif.gif"
             bird = bird + 1;
        } else {
-            img4.src = "hear.jpg"
+            img.src = "hear.jpg"
        }
     }
 }
